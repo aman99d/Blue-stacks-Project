b4c42c2a5e5a174006039f7e90f91ed85a1a0569
aman99d <83946565+aman99d@users.noreply.github.com>
2021-05-10 23:04:07 +0530

    Add files via upload

---
+++ project/locators.py
@@ -0,0 +1,7 @@
+class Locators:
+
+    xapth='adasd'
+    url="https://weather.com/en-IN/?Goto=Redirected"
+    search_box="//input[@id='LocationSearch_input']"
+    search_result="//button[text()='Delhi']"
+    tempature="(//span[@data-testid='TemperatureValue'])[1]"
